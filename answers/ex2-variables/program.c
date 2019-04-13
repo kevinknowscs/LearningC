d04f13e93c3a2d5db324795cd2da7f011c4979cf
Kevin Dietz <kdietz@atlassian.com>
2019-04-13 11:53:01 -0700

    Added ex2-variables answer

---
+++ answers/ex2-variables/program.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int a = 10;
+  int b;
+  int c = 20;
+
+  printf("a = %d\n", a);
+  printf("b = %d\n", b);
+  printf("c = %d\n", c);
+
+  int x = 10, y = 20, z = 30;
+
+  printf("x = %d\n", x);
+  printf("y = %d\n", y);
+  printf("z = %d\n", x);
+
+  return 0;
+}
